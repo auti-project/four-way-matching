de5a3110bc3460f2b4e39fe1f9f0ec61317f5050
txaty <xtianae@connect.ust.hk>
2023-01-15 05:35:05 +0800

    Add merkle proof.

--- fwmcircuit/fwm_circuit.go
+++ fwmcircuit/fwm_circuit.go
@@ -3,6 +3,7 @@ package fwmcircuit
 import (
 	twistededwards3 "github.com/consensys/gnark-crypto/ecc/twistededwards"
 	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/accumulator/merkle"
 	"github.com/consensys/gnark/std/algebra/twistededwards"
 	"github.com/consensys/gnark/std/hash/mimc"
 )
@@ -10,35 +11,36 @@ import (
 // FWMCircuit defines the four-way matching circuit
 type FWMCircuit struct {
 	// statements
-	Commitment1       twistededwards.Point `gnark:",public"`
-	Commitment2       twistededwards.Point `gnark:",public"`
-	Commitment3       twistededwards.Point `gnark:",public"`
-	Commitment4       twistededwards.Point `gnark:",public"`
-	AmountCommitment1 twistededwards.Point `gnark:",public"`
-	AmountCommitment4 twistededwards.Point `gnark:",public"`
-	//MerkleRoot1       frontend.Variable `gnark:",public"`
-	//MerkleRoot2       frontend.Variable `gnark:",public"`
-	//MerkleRoot3       frontend.Variable `gnark:",public"`
-	//MerkleRoot4       frontend.Variable `gnark:",public"`
-	//TimeStampRange    frontend.Variable `gnark:",public"`
-	// witnessesr
-	G          twistededwards.Point `gnark:"g"`
-	H          twistededwards.Point `gnark:"h"`
-	Counter    frontend.Variable    `gnark:"counter"`
-	Aux1       frontend.Variable    `gnark:"aux1"`
-	Aux2       frontend.Variable    `gnark:"aux2"`
-	Aux3       frontend.Variable    `gnark:"aux3"`
-	Aux4       frontend.Variable    `gnark:"aux4"`
-	Amount1    frontend.Variable    `gnark:"amount1"`
-	Amount4    frontend.Variable    `gnark:"amount4"`
-	Timestamp1 frontend.Variable    `gnark:"timestamp1"`
-	Timestamp2 frontend.Variable    `gnark:"timestamp2"`
-	Timestamp3 frontend.Variable    `gnark:"timestamp3"`
-	Timestamp4 frontend.Variable    `gnark:"timestamp4"`
-	//MerklePath1 frontend.Variable `gnark:"merkle_path1"`
-	//MerklePath2 frontend.Variable `gnark:"merkle_path2"`
-	//MerklePath3 frontend.Variable `gnark:"merkle_path3"`
-	//MerklePath4 frontend.Variable `gnark:"merkle_path4"`
+	Commitment1         twistededwards.Point `gnark:",public"`
+	Commitment2         twistededwards.Point `gnark:",public"`
+	Commitment3         twistededwards.Point `gnark:",public"`
+	Commitment4         twistededwards.Point `gnark:",public"`
+	AmountCommitment1   twistededwards.Point `gnark:",public"`
+	AmountCommitment4   twistededwards.Point `gnark:",public"`
+	TimestampRangeLeft  frontend.Variable    `gnark:"timestamp_range_left,public"`
+	TimestampRangeRight frontend.Variable    `gnark:"timestamp_range_right,public"`
+	Root1               frontend.Variable    `gnark:",public"`
+	Root2               frontend.Variable    `gnark:",public"`
+	Root3               frontend.Variable    `gnark:",public"`
+	Root4               frontend.Variable    `gnark:",public"`
+	// witnesses
+	G              twistededwards.Point `gnark:"g"`
+	H              twistededwards.Point `gnark:"h"`
+	Counter        frontend.Variable    `gnark:"counter"`
+	Aux1           frontend.Variable    `gnark:"aux1"`
+	Aux2           frontend.Variable    `gnark:"aux2"`
+	Aux3           frontend.Variable    `gnark:"aux3"`
+	Aux4           frontend.Variable    `gnark:"aux4"`
+	Amount1        frontend.Variable    `gnark:"amount1"`
+	Amount4        frontend.Variable    `gnark:"amount4"`
+	Timestamp1     frontend.Variable    `gnark:"timestamp1"`
+	Timestamp2     frontend.Variable    `gnark:"timestamp2"`
+	Timestamp3     frontend.Variable    `gnark:"timestamp3"`
+	Timestamp4     frontend.Variable    `gnark:"timestamp4"`
+	Path1, Helper1 []frontend.Variable
+	Path2, Helper2 []frontend.Variable
+	Path3, Helper3 []frontend.Variable
+	Path4, Helper4 []frontend.Variable
 }
 
 // Define declares the circuit constraints
@@ -68,6 +70,19 @@ func (circuit *FWMCircuit) Define(api frontend.API) error {
 	circuit.assertCommit(api, &curve, &mimcHash, circuit.Aux2, circuit.Timestamp2, circuit.Commitment2)
 	circuit.assertCommit(api, &curve, &mimcHash, circuit.Aux3, circuit.Timestamp3, circuit.Commitment3)
 	circuit.assertCommit(api, &curve, &mimcHash, circuit.Aux4, circuit.Timestamp4, circuit.Commitment4)
+
+	circuit.assertTimestampRange(api, circuit.Timestamp1)
+	circuit.assertTimestampRange(api, circuit.Timestamp2)
+	circuit.assertTimestampRange(api, circuit.Timestamp3)
+	circuit.assertTimestampRange(api, circuit.Timestamp4)
+
+	merkle.VerifyProof(api, mimcHash, circuit.Root1, circuit.Path1, circuit.Helper1)
+	mimcHash.Reset()
+	merkle.VerifyProof(api, mimcHash, circuit.Root2, circuit.Path2, circuit.Helper2)
+	mimcHash.Reset()
+	merkle.VerifyProof(api, mimcHash, circuit.Root3, circuit.Path3, circuit.Helper3)
+	mimcHash.Reset()
+	merkle.VerifyProof(api, mimcHash, circuit.Root4, circuit.Path4, circuit.Helper4)
 	return nil
 }
 
@@ -86,4 +101,9 @@ func (circuit *FWMCircuit) assertCommit(api frontend.API, curve *twistededwards.
 	numCommit = (*curve).Add(numCommit, tmp)
 	api.AssertIsEqual(commit.X, numCommit.X)
 	api.AssertIsEqual(commit.Y, numCommit.Y)
+}
+
+func (circuit *FWMCircuit) assertTimestampRange(api frontend.API, timestamp frontend.Variable) {
+	api.AssertIsLessOrEqual(circuit.TimestampRangeLeft, timestamp)
+	api.AssertIsLessOrEqual(timestamp, circuit.TimestampRangeRight)
 }
--- main.go
+++ main.go
@@ -2,29 +2,31 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
 	"fmt"
 	"math/big"
 	"time"
 
 	"github.com/auti-project/four-way-matching/fwmcircuit"
+	"github.com/consensys/gnark-crypto/accumulator/merkletree"
 	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr/mimc"
 	"github.com/consensys/gnark-crypto/ecc/bn254/twistededwards"
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/std/accumulator/merkle"
 	twistededwards2 "github.com/consensys/gnark/std/algebra/twistededwards"
 )
 
+func hashFunc(data []byte) ([]byte, error) {
+	mimcHash := mimc.NewMiMC()
+	mimcHash.Write(data)
+	return mimcHash.Sum(nil), nil
+}
+
 func main() {
-	// compiles our circuit into a R1CS
-	var circuit fwmcircuit.FWMCircuit
-	ccs, err := frontend.Compile(ecc.BN254, r1cs.NewBuilder, &circuit)
-	handleErr(err)
-
-	// groth16 zkSNARK: Setup
-	pk, vk, err := groth16.Setup(ccs)
-	handleErr(err)
-
 	var (
 		amount1 int64 = 100
 		amount4 int64 = 200
@@ -35,6 +37,8 @@ func main() {
 		counter int64 = 10
 	)
 
+	timestampRangeLeft := time.Now().UnixNano()
+	time.Sleep(200 * time.Millisecond)
 	timestamp1 := time.Now().UnixNano()
 	time.Sleep(200 * time.Millisecond)
 	timestamp2 := time.Now().UnixNano()
@@ -42,6 +46,8 @@ func main() {
 	timestamp3 := time.Now().UnixNano()
 	time.Sleep(200 * time.Millisecond)
 	timestamp4 := time.Now().UnixNano()
+	time.Sleep(200 * time.Millisecond)
+	timestampRangeRight := time.Now().UnixNano()
 
 	privateG, privateH, err := fwmcircuit.KeyGen()
 	if err != nil {
@@ -57,44 +63,177 @@ func main() {
 		X: h.X.String(),
 		Y: h.Y.String(),
 	}
-	//amountCommit1 := new(twistededwards.PointAffine).ScalarMul(g, big.NewInt(amount1))
-	//tmp := new(twistededwards.PointAffine).ScalarMul(
-	//	h,
-	//	fwmcircuit.MiMCHashTimestampCounter(timestamp1, counter),
-	//)
-	//amountCommit1.Add(amountCommit1, tmp)
-	//assignAmountCommit1 := twistededwards2.Point{
-	//	X: amountCommit1.X.String(),
-	//	Y: amountCommit1.Y.String(),
-	//}
-	assignCommitAmount1 := assignCommit(amount1, timestamp1, counter, g, h)
-	assignCommitAmount4 := assignCommit(amount4, timestamp4, counter, g, h)
-	assignCommitAux1 := assignCommit(aux1, timestamp1, counter, g, h)
-	assignCommitAux2 := assignCommit(aux2, timestamp2, counter, g, h)
-	assignCommitAux3 := assignCommit(aux3, timestamp3, counter, g, h)
-	assignCommitAux4 := assignCommit(aux4, timestamp4, counter, g, h)
+	assignCommitAmount1, _ := assignCommit(amount1, timestamp1, counter, g, h)
+	assignCommitAmount4, _ := assignCommit(amount4, timestamp4, counter, g, h)
+	assignCommitAux1, commitAux1 := assignCommit(aux1, timestamp1, counter, g, h)
+	assignCommitAux2, commitAux2 := assignCommit(aux2, timestamp2, counter, g, h)
+	assignCommitAux3, commitAux3 := assignCommit(aux3, timestamp3, counter, g, h)
+	assignCommitAux4, commitAux4 := assignCommit(aux4, timestamp4, counter, g, h)
+
+	var buf bytes.Buffer
+	for i := 0; i < 10000; i++ {
+		if i == 100 {
+			buf.Write(commitAux1)
+		} else if i == 200 {
+			buf.Write(commitAux2)
+		} else if i == 300 {
+			buf.Write(commitAux3)
+		} else if i == 400 {
+			buf.Write(commitAux4)
+		} else {
+			randBuf := make([]byte, 32)
+			_, err := rand.Read(randBuf)
+			if err != nil {
+				panic(err)
+			}
+			buf.Write(randBuf)
+		}
+	}
+	root1, proof1, numLeaves, err := merkletree.BuildReaderProof(&buf, mimc.NewMiMC(), 32, 100)
+	proofHelper1 := merkle.GenerateProofHelper(proof1, 100, numLeaves)
+	buf.Reset()
+	for i := 0; i < 10000; i++ {
+		if i == 100 {
+			buf.Write(commitAux1)
+		} else if i == 200 {
+			buf.Write(commitAux2)
+		} else if i == 300 {
+			buf.Write(commitAux3)
+		} else if i == 400 {
+			buf.Write(commitAux4)
+		} else {
+			randBuf := make([]byte, 32)
+			_, err := rand.Read(randBuf)
+			if err != nil {
+				panic(err)
+			}
+			buf.Write(randBuf)
+		}
+	}
+	root2, proof2, numLeaves, err := merkletree.BuildReaderProof(&buf, mimc.NewMiMC(), 32, 200)
+	proofHelper2 := merkle.GenerateProofHelper(proof2, 200, numLeaves)
+	buf.Reset()
+	for i := 0; i < 10000; i++ {
+		if i == 100 {
+			buf.Write(commitAux1)
+		} else if i == 200 {
+			buf.Write(commitAux2)
+		} else if i == 300 {
+			buf.Write(commitAux3)
+		} else if i == 400 {
+			buf.Write(commitAux4)
+		} else {
+			randBuf := make([]byte, 32)
+			_, err := rand.Read(randBuf)
+			if err != nil {
+				panic(err)
+			}
+			buf.Write(randBuf)
+		}
+	}
+	root3, proof3, numLeaves, err := merkletree.BuildReaderProof(&buf, mimc.NewMiMC(), 32, 300)
+	proofHelper3 := merkle.GenerateProofHelper(proof3, 300, numLeaves)
+	buf.Reset()
+	for i := 0; i < 10000; i++ {
+		if i == 100 {
+			buf.Write(commitAux1)
+		} else if i == 200 {
+			buf.Write(commitAux2)
+		} else if i == 300 {
+			buf.Write(commitAux3)
+		} else if i == 400 {
+			buf.Write(commitAux4)
+		} else {
+			randBuf := make([]byte, 32)
+			_, err := rand.Read(randBuf)
+			if err != nil {
+				panic(err)
+			}
+			buf.Write(randBuf)
+		}
+	}
+	root4, proof4, numLeaves, err := merkletree.BuildReaderProof(&buf, mimc.NewMiMC(), 32, 400)
+	proofHelper4 := merkle.GenerateProofHelper(proof4, 400, numLeaves)
+
+	// compiles our circuit into a R1CS
+	var circuit fwmcircuit.FWMCircuit = fwmcircuit.FWMCircuit{
+		Path1:   make([]frontend.Variable, len(proof1)),
+		Path2:   make([]frontend.Variable, len(proof2)),
+		Path3:   make([]frontend.Variable, len(proof3)),
+		Path4:   make([]frontend.Variable, len(proof4)),
+		Helper1: make([]frontend.Variable, len(proofHelper1)),
+		Helper2: make([]frontend.Variable, len(proofHelper2)),
+		Helper3: make([]frontend.Variable, len(proofHelper3)),
+		Helper4: make([]frontend.Variable, len(proofHelper4)),
+	}
+	ccs, err := frontend.Compile(ecc.BN254, r1cs.NewBuilder, &circuit)
+	handleErr(err)
+
+	// groth16 zkSNARK: Setup
+	pk, vk, err := groth16.Setup(ccs)
+	handleErr(err)
 
 	// witness definition
 	assignment := fwmcircuit.FWMCircuit{
-		G:                 assignG,
-		H:                 assignH,
-		Counter:           counter,
-		Commitment1:       assignCommitAux1,
-		Commitment2:       assignCommitAux2,
-		Commitment3:       assignCommitAux3,
-		Commitment4:       assignCommitAux4,
-		AmountCommitment1: assignCommitAmount1,
-		AmountCommitment4: assignCommitAmount4,
-		Aux1:              aux1,
-		Aux2:              aux2,
-		Aux3:              aux3,
-		Aux4:              aux4,
-		Amount1:           amount1,
-		Amount4:           200,
-		Timestamp1:        timestamp1,
-		Timestamp2:        timestamp2,
-		Timestamp3:        timestamp3,
-		Timestamp4:        timestamp4,
+		// statements
+		Commitment1:         assignCommitAux1,
+		Commitment2:         assignCommitAux2,
+		Commitment3:         assignCommitAux3,
+		Commitment4:         assignCommitAux4,
+		AmountCommitment1:   assignCommitAmount1,
+		AmountCommitment4:   assignCommitAmount4,
+		TimestampRangeLeft:  timestampRangeLeft,
+		TimestampRangeRight: timestampRangeRight,
+		Root1:               root1,
+		Root2:               root2,
+		Root3:               root3,
+		Root4:               root4,
+		// witnesses
+		G:          assignG,
+		H:          assignH,
+		Counter:    counter,
+		Aux1:       aux1,
+		Aux2:       aux2,
+		Aux3:       aux3,
+		Aux4:       aux4,
+		Amount1:    amount1,
+		Amount4:    amount4,
+		Timestamp1: timestamp1,
+		Timestamp2: timestamp2,
+		Timestamp3: timestamp3,
+		Timestamp4: timestamp4,
+		Path1:      make([]frontend.Variable, len(proof1)),
+		Path2:      make([]frontend.Variable, len(proof2)),
+		Path3:      make([]frontend.Variable, len(proof3)),
+		Path4:      make([]frontend.Variable, len(proof4)),
+		Helper1:    make([]frontend.Variable, len(proofHelper1)),
+		Helper2:    make([]frontend.Variable, len(proofHelper2)),
+		Helper3:    make([]frontend.Variable, len(proofHelper3)),
+		Helper4:    make([]frontend.Variable, len(proofHelper4)),
+	}
+	for i := range proof1 {
+		assignment.Path1[i] = proof1[i]
+	}
+	for i := range proof2 {
+		assignment.Path2[i] = proof2[i]
+	}
+	for i := range proof3 {
+		assignment.Path3[i] = proof3[i]
+	}
+	for i := range proof4 {
+		assignment.Path4[i] = proof4[i]
+	}
+	for i := range proofHelper1 {
+		assignment.Helper1[i] = proofHelper1[i]
+	}
+	for i := range proofHelper2 {
+		assignment.Helper2[i] = proofHelper2[i]
+	}
+	for i := range proofHelper3 {
+		assignment.Helper3[i] = proofHelper3[i]
+	}
+	for i := range proofHelper4 {
+		assignment.Helper4[i] = proofHelper4[i]
 	}
 	fmt.Println("test hash", fwmcircuit.MiMCHashTimestampCounter(timestamp1, counter).String())
 	witness, err := frontend.NewWitness(&assignment, ecc.BN254)
@@ -109,14 +248,14 @@ func main() {
 
 }
 
-func assignCommit(num, timestamp, counter int64, g, h *twistededwards.PointAffine) twistededwards2.Point {
+func assignCommit(num, timestamp, counter int64, g, h *twistededwards.PointAffine) (twistededwards2.Point, []byte) {
 	commit := new(twistededwards.PointAffine).ScalarMul(g, big.NewInt(num))
 	tmp := new(twistededwards.PointAffine).ScalarMul(h, fwmcircuit.MiMCHashTimestampCounter(timestamp, counter))
 	commit.Add(commit, tmp)
 	return twistededwards2.Point{
 		X: commit.X.String(),
 		Y: commit.Y.String(),
-	}
+	}, commit.Marshal()
 }
 
 func handleErr(err error) {
